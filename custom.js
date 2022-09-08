49646b05036ad89957da38d38347ec0b96f91727
GullSherKhan <gullsher.khan@devsinc.com>
2022-09-08 20:00:21 +0500

    fixed the bug where the ripple effect was not working for pages with long height

--- custom.js
+++ custom.js
@@ -2,6 +2,7 @@
 
 // ripple => addKeyFrames must be called once before calling ripple function, creates ripple effect on the provided element.
 //        => ele - for element on which ripple effect is needed, time - needed to specify time after which the animation disappears, clas - specific class for ripple must be same as that included in css for ripple effect to work
+//        => endwait - flag to wait for the animation to end or not
 const ripple = (ele, clas = "ripple", endWait = true, hidetime = 500) =>
 {
   ele.onclick = function (e) {
@@ -11,11 +12,11 @@ const ripple = (ele, clas = "ripple", endWait = true, hidetime = 500) =>
     }
     let ripple = document.createElement("span");
     ripple.classList.add(clas);
-    ele.appendChild(ripple);
-    let x = e.clientX - e.target.offsetLeft;
-    let y = e.clientY - e.target.offsetTop;
+    let x = e.clientX - (e.target.offsetLeft - window.scrollX);
+    let y = e.clientY - (e.target.offsetTop - window.scrollY);
     ripple.style.left = `${x}px`;
     ripple.style.top = `${y}px`;
+    ele.appendChild(ripple);
     setTimeout(() => {
         ripple.remove();
     }, hidetime);
@@ -23,3 +24,4 @@ const ripple = (ele, clas = "ripple", endWait = true, hidetime = 500) =>
 }
 
 export {ripple}
+
